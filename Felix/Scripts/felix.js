6dbf0e066fa48594d186763a0a7dbd5e3829bd04
MnHung <MnHung@gmail.com>
2013-03-20 20:26:02 +0800

    start making form layout.

--- Felix/Scripts/felix.js
+++ Felix/Scripts/felix.js
@@ -1,6 +1,8 @@
 ﻿/// <reference path="plugins/jquery-1.9.1.js" />
 /// <reference path="plugins/jquery-ui-1.10.1.custom/js/jquery-ui-1.10.1.custom.js" />
 /// <reference path="plugins/jquery.jqGrid.src.js" />
+/// <reference path="plugins/grid.locale-tw.js" />
+/// <reference path="plugins/jquery.tmpl.js" />
 
 
 var Felix = {};
@@ -13,11 +15,15 @@ var print = function (content) {
 
 Felix.View.List = (function () {
 	var initial = function () {
+
+		// build list with jqGrid.
 		$("#felix-list").jqGrid({
 			url: 'test',
 			datatype: "local",
-			colNames: ['Name', 'Number'],
+			colNames: ['id', 'Name', 'Number'],
 			colModel: [
+			//hidden
+				{name: '_id', index: '_id', width: 90, hidden: true },
 				{ name: 'Name', index: 'Name', width: 90 },
 				{ name: 'Number', index: 'Number', width: 100 }
 			],
@@ -26,8 +32,29 @@ Felix.View.List = (function () {
 			sortname: 'id',
 			viewrecords: true,
 			sortorder: "desc",
-			caption: "JSON Example"
+			autowidth: true,
+			caption: "維修單",
+			pager: '#felix-list-pager',
+			onSelectRow: function (index) {
+				var data = $("#felix-list").getRowData(index);
+				Felix.View.Operation.Edit.renderFelixForm(data);
+			}
 		});
+
+		//		$("#felix-list").jqGrid({
+		//			onSelectRow: function(id){ 
+		//				print("id: " + id);	
+		//			}
+		//		});
+
+		// build jqGrid nav bar
+		//		$("#felix-list").jqGrid('navGrid', '#felix-list-pager',
+		//			{}, //options
+		//			{height: 280, reloadAfterSubmit: false }, // edit options
+		//			{height: 280, reloadAfterSubmit: false }, // add options
+		//			{reloadAfterSubmit: false }, // del options
+		//			{} // search options
+		//		);
 
 		// get felix data list to show.
 		$.getJSON("test", function (felixList) {
@@ -37,7 +64,29 @@ Felix.View.List = (function () {
 					$grid.jqGrid('addRowData', i + 1, felixList[i]);
 				}
 			}
+
+			// create dynamic edit button: 
+//			$("#felix-list tr").click(function (event) {
+//				var $row = $(event.currentTarget);
+//				var $firstCol = $row.find("td:first");
+//				print($firstCol);
+//			});
+
 		});
+
+		//		$(window).bind('resize', function () {
+		//			$("#felix-list").setGridWidth($(window).width() - 35);
+		//		}).trigger('resize');
+	}
+
+	return {
+		initial: initial
+	};
+} ());
+
+Felix.View.Operation = (function () {
+	var initial = function () {
+		$("#felix-operations").tabs({ });
 	};
 
 	return {
@@ -45,8 +94,23 @@ Felix.View.List = (function () {
 	};
 } ());
 
+Felix.View.Operation.Edit = (function () {
+	var renderFelixForm = function (felixData) {
+		print(felixData);
+		print($("#tmpl-felix-form").tmpl(felixData));
+		$("#tmpl-felix-form").tmpl(felixData).replaceAll("#felix-edit>div");
+	};
+
+	return {
+		renderFelixForm: renderFelixForm
+	};
+} ());
+
 Felix.start = function () {
-	$("#main").load("List", Felix.View.List.initial);
+	$("#felix-main")
+		.find(".list-panel").load("List", Felix.View.List.initial)
+		.end()
+		.find(".operation-panel").load("Operation", Felix.View.Operation.initial);
 
 };
 
